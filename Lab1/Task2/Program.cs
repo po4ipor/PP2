508867ef43db021f355e9e7ada69f3e512633d2d
po4ipor <Zaur.yeltai0412@gmail.com>
2019-02-15 12:59:34 +0600

    Комментарии тоже отсутсвует

--- Lab1/Task2/Program.cs
+++ Lab1/Task2/Program.cs
@@ -2,11 +2,31 @@
 
 namespace Task2
 {
-    class Program
-    {
-        static void Main(string[] args)
+        class Program
         {
-            Console.WriteLine("Hello World!");
+            static void Main(string[] args)
+            {
+                Student Zaur = new Student("Zaur", "18BD110400");//добавление нового студента
+                Zaur.year = 1;// ввод данных в переменную year
+                Zaur.Info();
+            }
         }
-    }
+        class Student // конструктор
+        {
+            public string name;
+            public string id;
+            public int year;
+            public Student(string n, string i)
+            {
+                name = n;
+                id = i;
+            }
+
+            public void Info()// вывод данных о студенте
+            {
+                Console.WriteLine($"{name} || {id} || {year}");
+                Console.ReadKey();
+            }
+        }
+    
 }
